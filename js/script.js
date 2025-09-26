5998136ed92d3854d1f5ee4ca645c9a7cf9890c5
Cal-vincoder22 <mphonetshidzati1@gmail.com>
2025-09-26 20:44:16 +0200

    poe part 2 submission

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Wait for the entire HTML document to be fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+  
+  // Log a message to the console indicating that the website has loaded successfully
+  console.log("Website loaded successfully!");
+  
+  // Display a pop-up alert welcoming the user to the website
+  alert("Welcome to Mhluri's Hair Salon website!");
+});
